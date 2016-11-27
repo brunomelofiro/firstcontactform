33960f486903957f8d64c142c6e2731fb3fb8195
Bruno Melofiro <bruno@brunomelofiro.com>
2016-11-26 23:49:16 -0500

    change footer and header settings

--- inc/styles.php
+++ inc/styles.php
@@ -57,7 +57,7 @@ function astrid_custom_styles($custom) {
 	$site_title = get_theme_mod( 'site_title', '#ffffff' );
 	$custom .= ".site-title a,.site-title a:hover { color:" . esc_attr($site_title) . "}"."\n";
 	$site_desc = get_theme_mod( 'site_description', '#BDBDBD' );
-	$custom .= ".site-description { color:000" . esc_attr($site_desc) . "}"."\n";
+	$custom .= ".site-description { color:#000" . esc_attr($site_desc) . "}"."\n";
 
 	$menu_bg    = get_theme_mod( 'menu_bg', '#000' );
 	$menu_rgba 	= astrid_hex2rgba($menu_bg, 0.9);
